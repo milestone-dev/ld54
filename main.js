c3e310ceea861b5131b0d9d4c34fc0080c6d3352
Milestone <games@kallepersson.se>
2023-09-30 21:44:32 +0200

    .

--- main.js
+++ main.js
@@ -29,6 +29,14 @@ const init = function() {
 	id("suitcase-lock-1").addEventListener("scroll", (evt) => { if (!isTouching) checkSuitcaseCode(); })
 	id("suitcase-lock-2").addEventListener("scroll", (evt) => { if (!isTouching) checkSuitcaseCode(); })
 	id("suitcase-lock-3").addEventListener("scroll", (evt) => { if (!isTouching) checkSuitcaseCode(); })
+
+	openSocketConnection();
+	if(window.location.search.indexOf("?p=") != -1) {
+		let parts = window.location.search.split("?p=");
+		if (parts.length > 1) {
+			startGame(parseInt(parts[1]));
+		}
+	}
 };
 
 const id = function(id) { return document.getElementById(id); }
@@ -51,7 +59,7 @@ const touchClick = function(evt) {
 	}
 
 	const id = evt.target.id;
-	if (id == "invite-p2" && navigator.share) navigator.share({url:window.location.href});
+	if (id == "invite-p2" && navigator.share) navigator.share({url:window.location.href + "?player=2"});
 	else if (id == "start-p1") startGame(1);
 	else if (id == "start-p2") startGame(2);
 }
@@ -90,6 +98,7 @@ const startGame = function(playerID) {
 	else if (playerID == 2) switchScreen("weather-symbols");
 	show(playerCanvas);
 	hide(id("start"));
+	sendSocketAction("start-game", {playerID: playerID});
 }
 
 const switchScreen = function(id) {
@@ -104,12 +113,40 @@ const checkSuitcaseCode = function() {
 	const l2 = Math.floor(id("suitcase-lock-2").scrollTop/id("suitcase-lock-2").offsetHeight);
 	const l3 = Math.floor(id("suitcase-lock-3").scrollTop/id("suitcase-lock-3").offsetHeight);
 	id("debug").innerText = `${l1} ${l2} ${l3}`;
-	if (l1 == 2 && l2 == 6 && l3 == 4) switchScreen("win");
+	if (l1 == 2 && l2 == 6 && l3 == 4) {
+		switchScreen("win");
+	}
 }
 
 const openSocketConnection = function() {
-	socketConnection = new WebSocket("ws://localhost:6502", "json");
-	console.log(socketConnection);
+	var url = "ws://46.21.104.84:6502";
+	if (window.location.href.indexOf("localhost") != -1) {
+		url = "ws://localhost:6502";
+	}
+	socketConnection = new WebSocket(url, "json");
+	socketConnection.onmessage = socketResponse;
+}
+
+const sendSocketAction = function(actionID, args) {
+	if (!socketConnection) return;
+	if (socketConnection.readyState == 0) {
+		socketConnection.onopen = function(evt) {
+			socketConnection.send(JSON.stringify({action:actionID, args:args}));
+		}
+	} else if (socketConnection.readyState == 1) {
+		socketConnection.send(JSON.stringify({action:actionID, args:args}));
+	}
+}
+
+const socketResponse = function(evt) {
+	const data = JSON.parse(evt.data);
+	const action = data.action;
+	const args = data.args;
+	switch(action) {
+		case "start-game":
+			if (args.playerID == 2) startGame(1);
+		break;
+	}
 }
 
 init();
---
+++ server.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var fs = require('fs');
+var WebSocketServer = require('websocket').server;
+var connections = [];
+var http = require('http');
+
+
+var httpServer = http.createServer({}, function(request, response) {
+	console.log((new Date()) + " Received request for " + request.url);
+	response.writeHead(404);
+	response.end();
+});
+
+httpServer.listen(6502, function() {
+	console.log((new Date()) + " Server is listening on port 6502");
+});
+
+console.log("***CREATING WEBSOCKET SERVER");
+var wsServer = new WebSocketServer({
+	httpServer: httpServer,
+	autoAcceptConnections: false
+});
+
+wsServer.on('request', function(request) {
+	// console.log(request, request.origin);
+	console.log("Request");
+
+	let connection = request.accept("json", request.origin);
+	let response = {
+		message: "hi",
+	};
+	connection.sendUTF(JSON.stringify(response));
+	connections.push(connection);
+
+	connection.on('message', function(message) {
+		if (message.type === 'utf8') {
+			console.log("Received Message: " + message.utf8Data);
+			const data = JSON.parse(message.utf8Data);
+			var dataResponse = {action:data.action, args:data.args};
+			connections.forEach(conn => {
+				if (conn != connection) {
+					conn.sendUTF(JSON.stringify(dataResponse));
+				}
+			});
+		}
+	});
+
+	connection.on('close', function(connection) {
+		connections = connections.filter(function(el, idx, ar) {
+			return el.connected;
+		});
+		console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+	});
+});
