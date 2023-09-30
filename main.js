e2966fe1c0e4e72d19b671ee785064fe261827f3
Milestone <games@kallepersson.se>
2023-09-30 13:31:14 +0200

    .

---
+++ main.js
@@ -0,0 +1,69 @@
+var playerCanvas;
+var currentScreen;
+
+const init = function() {
+	document.addEventListener("touchstart", touchStart);
+	document.addEventListener("touchend", touchEnd);
+	document.addEventListener("mouseup", touchEnd);
+	document.addEventListener("touchcancel", touchCancel);
+	document.addEventListener("touchmove", touchMove);
+
+	document.addEventListener("animationiteration", evt => {
+
+	});
+
+	document.addEventListener("animationend", evt => {
+
+	});
+
+	document.addEventListener("transitionend", evt => {
+		
+	});
+	console.log("ready");
+	startGame();
+};
+
+const elm = function(selector) { return document.querySelector(selector); }
+const elms = function(selector) { return document.querySelectorAll(selector); }
+const show = function(elm) { elm.classList.add("active"); }
+const hide = function(elm) { elm.classList.remove("active") }; 
+const hideAll = function(selector) {
+	elms(selector).forEach(function(elm) {
+		elm.classList.remove("active");
+	});
+}
+
+const touchStart = function(evt) {
+	console.log(evt, evt.target);
+}
+
+const touchEnd = function(evt) {
+	console.log(evt, evt.target);
+	let screenID = evt.target.dataset.screenId;
+	if (screenID) {
+		console.log(screenID);
+		switchScreen(screenID)
+	}
+}
+
+const touchCancel = function(evt) {
+	console.log(evt, evt.target);
+}
+
+const touchMove = function(evt) {
+	console.log(evt, evt.target);
+}
+
+const startGame = function() {
+	playerCanvas = elm("#p1");
+	show(playerCanvas);
+	switchScreen("suitcase");
+}
+
+const switchScreen = function(id) {
+	hideAll(".screen");
+	currentScreen = elm(`#${id}`);
+	show(currentScreen);
+}
+
+init();
